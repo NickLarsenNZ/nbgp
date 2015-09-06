6819de6ccb5d091038bdee5d38d69853483622d3
Nick Larsen <nick@aptiv.co.nz>
2015-09-06 20:15:06 +0900

    Managed to send an OPEN message using dummy code. Need to make it dynamic

--- nbgp.c
+++ nbgp.c
@@ -9,14 +9,18 @@
 //#include <sys/socket.h>
 #include <netdb.h>        // struct addrinfo
 
-
-
-
+bool      listening = false;
+bgp_peer  my;
+struct addrinfo standard_hints;
 
 int main(int argc, char *argv[]) {
 
   bgp_peer        *peer;
-  struct addrinfo standard_hints;
+  //struct addrinfo standard_hints; Moved outside of main().
+
+  my.as = 5507;
+  my.holdtime = 180;
+  my.id = inet_addr("172.16.78.2");
 
   // Set client side (us) hints.
   memset(&standard_hints, 0, sizeof(standard_hints));
@@ -29,8 +33,9 @@ int main(int argc, char *argv[]) {
 ///////////////////////////////////////
 // Temporarily populate the peers queue.
 
-  char *addresses[] = {"192.168.1.2", "192.168.1.3"};
-  int asns[] = {1400, 1401};
+  char *addresses[] = {"172.16.78.1", "172.16.78.3", "172.16.78.5"};
+  int asns[] = {1400, 1401, 1401};
+  bool passive[] = {false, false, true};
 
   int sizes = (int) (sizeof(addresses)/sizeof(char*));
 
@@ -45,6 +50,7 @@ int main(int argc, char *argv[]) {
 
     getaddrinfo(addresses[i], "bgp", &standard_hints, &peer->address);
     peer->as = asns[i];
+    peer->passive = passive[i];
 
     TAILQ_INSERT_TAIL(&bgp_peer_q, peer, entries);
 
@@ -54,13 +60,13 @@ int main(int argc, char *argv[]) {
 
 ///////////////////////////////////////
 
-
-  pthread_t server_cycle_thread;
   pthread_t client_cycle_thread;
-
-
+  pthread_t server_cycle_thread;
+  
+  pthread_create(&client_cycle_thread, NULL, start_client_cycle, NULL);
   pthread_create(&server_cycle_thread, NULL, start_server_cycle, NULL);
 
+  pthread_join(client_cycle_thread, NULL);
   pthread_join(server_cycle_thread, NULL);
 
 
@@ -92,9 +98,29 @@ char *friendly_ip(struct addrinfo *p) {
   return str;
 }
 
-void *start_server_cycle() {
+uint16_t friendly_port(struct addrinfo *p) {
+  uint16_t port;
+  
+  //
+  switch(p->ai_family) {
+    case AF_INET:
+      port = ((struct sockaddr_in *)p->ai_addr)->sin_port;
+      break;
+    case AF_INET6:
+      port = ((struct sockaddr_in6 *)p->ai_addr)->sin6_port;
+      break;
+  }
+
+  //inet_ntop(p->ai_family, addr, str, INET6_ADDRSTRLEN);
+
+  return ntohs(port);
+}
+
+void *start_client_cycle() {
 
   bgp_peer *next;
+
+  printf("Client cycle thread started...\n");
 
   next = TAILQ_FIRST(&bgp_peer_q);
   if(next == NULL) {
@@ -102,11 +128,11 @@ void *start_server_cycle() {
     return;
   }
   
-  printf("Server cycle thread\n");
+  
 
   for(;;) {
     
-    printf("Next address is %s\n", friendly_ip(next->address));
+    //printf("Next address is %s\n", friendly_ip(next->address));
 
     // Finite State Machine
     check_state(next);
@@ -116,8 +142,16 @@ void *start_server_cycle() {
     if(next == NULL) {
       next = TAILQ_FIRST(&bgp_peer_q);
     }
-    usleep(3000000);
+    usleep(500000); // Half a second sleep
   }
+}
+
+void *start_server_cycle() {
+  printf("Server cycle thread started...");
+  printf("Nothing to do, so exiting server cycle.\n");
+
+  // When listening, set listening to true.
+  listening = true;
 }
 
 // Finite State Machine
@@ -129,8 +163,115 @@ void check_state(bgp_peer *peer) {
       printf("Peer %s changed state to IDLE\n", friendly_ip(peer->address));
       break;
     case STATE_IDLE:
-
+      // If not a passive peer, try and connect:
+      if(peer->passive == false) {
+        peer->state = STATE_CONNECT;
+        printf("Peer %s changed state to CONNECT\n", friendly_ip(peer->address));
+        bgp_open(peer);
+      } else {
+        // If passive, the state should change to Active as soon as TCP connections are being accepted.
+        if(listening) {
+          peer->state = STATE_CONNECT;
+          printf("Peer %s changed state to ACTIVE\n", friendly_ip(peer->address));
+        }
+      } 
+      break;
+    case STATE_CONNECT:
+      //
+      
+      break;
+    case STATE_ACTIVE:
+      //
+      //peer->state = STATE_NULL;
       break;
   }
   return;
 }
+
+void bgp_open(bgp_peer *peer) {
+
+  struct bgp_header header;
+  struct bgp_msg_open open;
+
+  uint16_t message_length = sizeof(header) + sizeof(open);
+
+
+  open.version = 4;
+  open.as = htons(my.as);
+  open.holdtime = htons(my.holdtime);
+  open.id = my.id;
+  // Could add optional parameter TLVs here.
+  open.optlen = sizeof((struct bgp_opt_param*)open.params)/sizeof(open.params[0]);
+
+  printf("Checking opt values length. Expecting 0, got %ld\n", sizeof((struct bgp_opt_param*)open.params)/sizeof(open.params[0]));
+  
+  memset(&header.marker, 0xFF, sizeof(header.marker));
+  header.type = OPEN;
+  header.len =  htons(message_length);
+
+  char buffer[message_length];
+  printf("----> sizeof header %ld\n", sizeof(header));
+  printf("----> sizeof open %ld\n", sizeof(open));
+  printf("----> Made a buffer the size %d\n", message_length);
+  int pos = 0;
+  memcpy(buffer + pos, &header, sizeof(header));
+  pos += sizeof(header);
+  memcpy(buffer + pos, &open, sizeof(open));
+
+///////////////////////////////////////
+// Temporarily send a message to the peer.
+
+  int fd = socket(peer->address->ai_family, peer->address->ai_socktype, peer->address->ai_protocol);
+  if(fd == -1) {
+    perror("socket");
+    //exit(EXIT_FAILURE);
+  }
+  printf("Trying to connect to %s:%u\n", friendly_ip(peer->address), (unsigned int)friendly_port(peer->address));
+  if(connect(fd, peer->address->ai_addr, peer->address->ai_addrlen) != 0) {
+    perror("connect");
+    //exit(EXIT_FAILURE);
+  }
+  printf("Looks like we are connected to %s:%u\n", friendly_ip(peer->address), (unsigned int)friendly_port(peer->address));
+
+  // Now sent the OPEN message over the socket.
+  if(send(fd, buffer, message_length, 0) < 0) {
+    perror("send");
+    // Error counter?
+  }
+
+
+  // Then peer->state = STATE_OPENSENT
+
+/////////////////////////////////////// 
+
+
+
+  printf("Checking outer header length. Expecting 19, got %ld\n", sizeof(header));
+  printf("Checking OPEN message length (excl header). expecting 10, got %ld\n", sizeof(open));
+  printf("Total packet len: %d\n", header.len);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- nbgp.h
+++ nbgp.h
@@ -1,29 +1,92 @@
 //#include <sys/types.h>
 //#include <arpa/inet.h>
-#include <netdb.h>        // struct addrinfo
+#include <netdb.h>        // struct addrinfo.
 #include <sys/queue.h>    // TAILQ_* macros.
 
 #define BGP_PORT 179
 
-
-
+// Making a bool type since it's no part of standard C.
+typedef enum { false, true } bool;
 
 // BGP Peer Definition
 typedef struct bgp_peer {
-  TAILQ_ENTRY (bgp_peer)    entries;  // Pointers to the previous and next peers
-  struct addrinfo           *address; // AF Independent Peer
-  uint16_t                  as;       // Autonomous System Number (2 Bytes)
-  uint8_t                   state;    // BGP peer state
+  TAILQ_ENTRY (bgp_peer)    entries;  // Pointers to the previous and next peers.
+  struct addrinfo           *address; // AF Independent Peer.
+  uint16_t                  as;       // Autonomous System Number (2 Bytes).
+  uint8_t                   state;    // BGP peer state.
+  uint16_t                  holdtime; // 0, or >3, default is 180, in the peering session, the lowest hold time wins.
+  bool                      passive;  // Whether to attempt an outbound conneciton, or wait for incoming connections.
+  uint32_t                  id;       // BGP/Router ID. Host Byte Order, must change to Network Byte Order (htonl).
 } bgp_peer;
 
-TAILQ_HEAD(, bgp_peer) bgp_peer_q;    // Queue containing all of our peers
+TAILQ_HEAD(, bgp_peer) bgp_peer_q;    // Queue containing all of our peers.
 
+// Finite State Machine states.
 enum bgp_states {
   STATE_NULL,                         // Added this so I could do a NULL state check.
-  STATE_IDLE,
+  STATE_IDLE,                         // Initial peer state.
+  STATE_CONNECT,                      // When an outgoing TCP connection is being established.
+  STATE_ACTIVE,                       // When we are listening for incoming TCP connections to be established.
+  STATE_OPENSENT,                     // When a TCP socket is established, and OPEN message has been sent.
+  STATE_OPENCONFIRM,                  // When we get an OPEN reply from the peer.
+  STATE_ESTABLISHED                   // After checking everything is ok between the OPEN messages.
 };
 
+enum bgp_msg_types {
+  OPEN          = 1,
+  UPDATE        = 2,
+  NOTIFICATION  = 3,
+  KEEPALIVE     = 4
+};
+
+// Common BGP Header
+struct bgp_header {
+  u_char    marker[16];               // All ones.
+  uint16_t  len;                      // Total message length.
+  uint8_t   type;                     // The type of the following message (OPEN, UPDATE, NOTIFICATION, KEEPALIVE).
+} __attribute__((__packed__));        // Very important to have the packed attribute, otherwise padding will be added, making it hard to use sizeof on the struct.
+
+// TLVs
+struct bgp_opt_param {
+  uint8_t   type;                     // Param type.
+  uint8_t   len;                      // TLV length.
+  u_char   *value;                    // Variable length value.
+} __attribute__((__packed__));        // Very important to have the packed attribute, otherwise padding will be added, making it hard to use sizeof on the struct.
+
+// OPEN message
+struct bgp_msg_open {
+  uint8_t   version;                  // BGP version 4 (currently).
+  uint16_t  as;                       // 2-Byte Autonomous System Number (if using a 4-Byte ASN, set this to 23456, then send a capability message with the 4-Byte ASN).
+  uint16_t  holdtime;                 // 0, or >3, default is 180, in the peering session, the lowest hold time wins.
+  uint32_t  id;                       // BGP/Router ID.
+  uint8_t   optlen;                   // Optional Parameters Total Length.
+  struct    bgp_opt_param params[];   // Optional Paramater TLVs.
+} __attribute__((__packed__));        // Very important to have the packed attribute, otherwise padding will be added, making it hard to use sizeof on the struct.
+
 char *friendly_ip(struct addrinfo*);
+uint16_t friendly_port(struct addrinfo*);
 void *start_server_cycle();
 void *start_client_cycle();
 void check_state(bgp_peer*);
+void bgp_open(bgp_peer*);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
